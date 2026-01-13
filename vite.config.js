5fd26b2ecb5ab82cee737f6b5947d16ce3d6274f
developanther17 <developanther17@gmail.com>
2026-01-12 20:52:21 -0600

    pathresolve

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path' // Necesario para rutas absolutas
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacidad: resolve(__dirname, 'privacidad.html'),
+        terminos: resolve(__dirname, 'terminos.html'),
+      },
+    },
+  },
 })
